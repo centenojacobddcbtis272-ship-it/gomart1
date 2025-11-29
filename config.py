4500bacd586fb80e201704837c6098088fd0bb71
Diego CJ <centeno.jacob.dd.cbtis272@gmail.com>
2025-11-29 15:34:45 -0500

    Actualización

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-MONGO_URI = "mongodb+srv://centenojacobddcbtis272_db_user:facil123@cluster0.l4mbxlw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/gomart"
+MONGO_URI = "mongodb+srv://centenojacobddcbtis272_db_user:facil123@cluster0.l4mbxlw.mongodb.net/gomart?retryWrites=true&w=majority&appName=Cluster0"
+
 SECRET_KEY = "gomart_secret_key_2025"
